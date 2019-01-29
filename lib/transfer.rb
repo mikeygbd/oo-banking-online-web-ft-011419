912f2cdaa8d108e68446bdb202dfb12496bccd46
Michael Soares <mikey.soares@gmail.com>
2019-01-29 02:54:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -35,7 +35,7 @@ class Transfer
   end
 
   def reverse_transfer
-     if @status = "complete"
+     if @status == "complete"
     receiver.balance = receiver.balance - amount
     sender.deposit(amount)
     @status = "reversed"
