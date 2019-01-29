7d0ab5de089c02b724ddcc9ce3816e0e98c45628
Michael Soares <mikey.soares@gmail.com>
2019-01-29 01:50:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
   def execute_transaction
     receiver.deposit(amount)
     sender.balance = sender.balance - @amount
-    if !sender.valid? || !receiver.valid? || self == self
+    if !sender.valid? || !receiver.valid? 
 
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
